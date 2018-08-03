2267f7017f4c9a01f8c2c438f725aa6538000d4c
wenshan <shanshan1370@gmail.com>
2018-08-02 22:05:05 -0400

    visualization param

--- ImageFlow.py
+++ ImageFlow.py
@@ -184,7 +184,7 @@ def show(ang, mag, outDir = None, waitTime = None):
     # mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1]
 
     hsv[..., 0] = (ang + 180)/ 2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    hsv[..., 2] = np.clip(mag*10, 0, 255).astype(np.uint8) #cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
     bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
     if ( outDir is not None ):
